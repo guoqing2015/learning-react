946700d619d64b97603c998cf6007c3c5348446c
guoqing2013 <guoqing2013@163.com>
2018-04-30 17:41:16 +0800

    react.16

---
+++ tutorial/Leverage New Features of React 16/03 - Render Text Only Components in React 16/script.jsx
@@ -0,0 +1,21 @@
+
+const Comment = ({ text }) => {
+    const emojifiedText = text
+    .replace(':)', '😊')
+    .replace(':D', '😀')
+    .replace(':(', '🙁');
+    return emojifiedText;
+};
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <Comment text="Today we are sailing home :)" />
+        </div>
+      );
+    }
+}
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
+  
